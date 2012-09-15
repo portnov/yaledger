2b23472dacc653c01e5eb8cea2fa3899e42f55bc
Ilya Portnov <portnov@iportnov.ru>
2012-09-15 23:07:49 +0600

    Generic CSV parser.
    refs #80.

--- CSV.hs
+++ CSV.hs
@@ -2,106 +2,19 @@
 module Main where
 
 import Control.Monad
-import Data.Dates hiding (pDate)
-import Data.List.Utils
-import Text.Parsec
-import Text.Parsec.String
-import Text.Regex.PCRE
+import System.Environment
 
 import YaLedger.Types
 import YaLedger.Tree
 import YaLedger.Pretty
 import YaLedger.Parser
-
-pDate :: Parsec String () DateTime
-pDate = do
-  dayS <- many1 digit
-  char '/'
-  monthS <- many1 digit
-  char '/'
-  yearS <- many1 digit
-  return $ DateTime (read yearS) (read monthS) (read dayS) 0 0 0
-
-accountName = "карточка"
-currency = "р"
-
-categories =
-  [("ИНТЕРНЕТ", "интернет"),
-   ("премия",   "премия"),
-   ("Заработная плата", "зарплата"),
-   ("Аванс", "аванс"),
-   ("Выдача наличных", "наличные"),
-   ("Комиссия", "комиссии"),
-   ("Плата за обслуживание", "комиссии")]
-
-getCategory :: String -> Maybe String
-getCategory description =
-    case concatMap check categories of
-      [] -> Nothing
-      list -> Just $ head list
-  where
-    check (regexp, category) =
-      if description =~ regexp
-        then [category]
-        else []
-
-readSum str = read $ filter (/= ' ') str
-
-convert :: AccountPlan -> [String] -> IO (Ext Record)
-convert plan row = do
-  let dateStr     = row !! 0
-      description = row !! 2
-      amountStr   = row !! 3
-      attrs = [("description", description)] ++
-              case getCategory description of
-                Nothing -> []
-                Just category -> [("category", category)]
-
-  (s, amount) <- case head amountStr of
-                   '+' -> return (ECredit, readSum (tail amountStr))
-                   '-' -> return (EDebit,  readSum (tail amountStr))
-                   c   -> fail $ "Unknown amount sign: " ++ [c]
-  date <- case runParser pDate () "<stdin>" dateStr of
-            Right x -> return x
-            Left err -> fail $ show err
-
-  acc <- case lookupTree (mkPath accountName) plan of
-           [] -> fail $ "No such account: " ++ accountName
-           [a] -> return a
-           as -> fail "Ambigous account specification"
-
-  entry <- case s of
-             ECredit -> do
-               posting <- cposting acc (amount :# currency)
-               return $ UEntry [] [posting] Nothing []
-             EDebit -> do
-               posting <- dposting acc (amount :# currency)
-               return $ UEntry [posting] [] Nothing []
-  return $ Ext date attrs (Transaction $ TEntry entry)
-
-cposting :: AnyAccount -> Amount -> IO (Posting Amount Credit)
-cposting acc x =
-  case acc of
-    WCredit _ a -> return $ CPosting (Right a) x
-    WFree   _ a -> return $ CPosting (Left  a) x
-    WDebit  _ _ -> fail $ "Invalid account type: debit instead of credit"
-
-dposting :: AnyAccount -> Amount -> IO (Posting Amount Debit)
-dposting acc x =
-  case acc of
-    WDebit  _ a -> return $ DPosting (Right a) x
-    WFree   _ a -> return $ DPosting (Left  a) x
-    WCredit  _ _ -> fail $ "Invalid account type: credit instead of debit"
-
-parseRow :: String -> [String]
-parseRow line = split ";" line
+import YaLedger.Parser.CSV
 
 main = do
+  [config, path] <- getArgs
   plan <- readPlan "test.accounts"
-  string <- getContents
-  let csv = map parseRow $ lines string
-  forM_ csv $ \row -> do
-    record <- convert plan row
-    putStrLn $ prettyPrint record
+  records <- loadCSV plan config path
+  forM_ records $ \rec ->
+    putStrLn $ prettyPrint rec
 
 
---
+++ YaLedger/Parser/CSV.hs
@@ -0,0 +1,194 @@
+{-# LANGUAGE OverloadedStrings #-}
+module YaLedger.Parser.CSV where
+
+import Control.Applicative
+import Control.Monad
+import qualified Data.Text as T
+import qualified Data.HashMap.Strict as H
+import qualified Data.ByteString as B
+import Data.String
+import Data.Yaml
+import Data.String.Utils
+import Data.Dates.Formats hiding (Fixed)
+import Text.Regex.PCRE
+
+import YaLedger.Types
+import YaLedger.Tree
+import YaLedger.Parser
+
+data FieldConfig =
+    FieldConfig {
+      fcField :: Int,
+      fcRegexp :: Maybe String,
+      fcRules :: [(String, String)] }
+  | FixedValue String
+  deriving (Eq, Show)
+
+defaultFieldConfig :: Int -> FieldConfig
+defaultFieldConfig n =
+  FieldConfig {
+    fcField = n,
+    fcRegexp = Nothing,
+    fcRules = [] }
+
+data ParserConfig = ParserConfig {
+    pcSeparator :: String,
+    pcDateFormat :: String,
+    pcDate :: FieldConfig,
+    pcCurrency :: FieldConfig,
+    pcAmount :: FieldConfig,
+    pcAccount :: FieldConfig,
+    pcAccount2 :: Maybe FieldConfig,
+    pcOther :: [(String, FieldConfig)]
+    }
+  deriving (Eq, Show)
+
+instance FromJSON FieldConfig where
+  parseJSON (Object v) =
+    FieldConfig
+      <$> v .: "field"
+      <*> v .:? "regexp"
+      <*> getRules v
+  parseJSON (String str) =
+    pure $ FixedValue (T.unpack str)
+  parseJSON _ = fail "Invalid object"
+
+getRules :: Object -> Parser [(String, String)]
+getRules obj = do
+  rec <- obj .:? "rules"
+  case rec of
+    Nothing -> return []
+    Just (Object rules) ->
+        forM (H.toList rules) $ \(name, value) ->
+          case value of
+            String str -> return (T.unpack name, T.unpack str)
+            _ -> mzero
+
+instance FromJSON ParserConfig where
+  parseJSON (Object v) =
+    ParserConfig
+      <$> v .:? "separator" .!= ","
+      <*> v .:? "dateformat" .!= "YYYY/MM/DD"
+      <*> v .:  "date"
+      <*> v .:? "currency"  .!= FixedValue "$"
+      <*> v .:  "amount"
+      <*> v .:  "account"
+      <*> v .:? "account2"
+      <*> getOther v
+
+reservedFields :: IsString s => [s]
+reservedFields =
+  ["separator", "currency", "amount",
+   "account", "account2",
+   "dateformat", "date"]
+
+getOther :: Object -> Parser [(String, FieldConfig)]
+getOther obj = do
+  let pairs = H.toList obj
+      pairs' = filter (\(name,_) -> name `notElem` reservedFields) pairs
+  forM pairs' $ \(name, value) -> do
+      fc <- parseJSON value
+      return (T.unpack name, fc)
+
+loadParserConfig :: FilePath -> IO ParserConfig
+loadParserConfig path = do
+  str <- B.readFile path
+  case decode str of
+    Nothing -> fail $ "Cannot parse config file " ++ path
+    Just pc -> return pc
+
+csv :: String -> String -> [[String]]
+csv sep str = map parseRow $ lines str
+  where
+    parseRow = split sep
+
+parseCSV :: ParserConfig -> AccountPlan -> String -> IO [Ext Record]
+parseCSV pc plan str = mapM (convert pc plan) $ csv (pcSeparator pc) str
+
+field :: FieldConfig -> [String] -> String
+field (FixedValue str) _ = str
+field fc row =
+  let str = row !! (fcField fc - 1)
+      part = case fcRegexp fc of
+               Nothing -> str
+               Just regexp -> str =~ regexp
+      check (res, regexp) =
+          if part =~ regexp
+            then [res]
+            else []
+  in  case fcRules fc of
+        [] -> part
+        rules -> case concatMap check rules of
+                   [] -> ""
+                   (x:_) -> x
+
+readSum str = read $ filter (/= ' ') str
+
+convert :: ParserConfig -> AccountPlan -> [String] -> IO (Ext Record)
+convert pc plan row = do
+  let dateStr = field (pcDate pc) row
+      currency = field (pcCurrency pc) row
+      amountStr = field (pcAmount pc) row
+      accountName = field (pcAccount pc) row
+      account2 = case pcAccount2 pc of
+                   Just fc -> Just $ field fc row
+                   Nothing -> Nothing
+      as = [(name, field fc row) | (name, fc) <- pcOther pc,
+                                    name `notElem` reservedFields]
+      attrs = filter (\p -> not $ null (snd p)) as
+
+      lookupAccount path =
+         case lookupTree (mkPath path) plan of
+           [] -> fail $ "No such account: " ++ path
+           [a] -> return a
+           as -> fail "Ambigous account specification"
+
+  date <- case parseDateFormat (pcDateFormat pc) dateStr of
+            Right d -> return d
+            Left err -> fail $ show err
+
+  (s, amount) <- case head amountStr of
+                   '+' -> return (ECredit, readSum (tail amountStr))
+                   '-' -> return (EDebit,  readSum (tail amountStr))
+                   _   -> return (ECredit, readSum amountStr)
+
+  acc <- lookupAccount accountName
+  acc2 <- case account2 of
+            Nothing -> return Nothing
+            Just path -> Just <$> lookupAccount path
+
+  entry <- case s of
+             ECredit -> do
+               posting <- cposting acc (amount :# currency)
+               corr <- case acc2 of
+                         Just acc -> dposting acc (amount :# currency)
+                         Nothing  -> return []
+               return $ UEntry corr posting Nothing []
+             EDebit -> do
+               posting <- dposting acc (amount :# currency)
+               corr <- case acc2 of
+                         Just acc -> cposting acc (amount :# currency)
+                         Nothing  -> return []
+               return $ UEntry posting corr Nothing []
+  return $ Ext date attrs (Transaction $ TEntry entry)
+
+cposting :: AnyAccount -> Amount -> IO [Posting Amount Credit]
+cposting acc x =
+  case acc of
+    WCredit _ a -> return [CPosting (Right a) x]
+    WFree   _ a -> return [CPosting (Left  a) x]
+    WDebit  _ _ -> fail $ "Invalid account type: debit instead of credit"
+
+dposting :: AnyAccount -> Amount -> IO [Posting Amount Debit]
+dposting acc x =
+  case acc of
+    WDebit  _ a -> return [DPosting (Right a) x]
+    WFree   _ a -> return [DPosting (Left  a) x]
+    WCredit  _ _ -> fail $ "Invalid account type: credit instead of debit"
+
+loadCSV :: AccountPlan -> FilePath -> FilePath -> IO [Ext Record]
+loadCSV plan configPath csvPath = do
+  config <- loadParserConfig configPath 
+  csv <- readFile csvPath
+  parseCSV config plan csv
+
