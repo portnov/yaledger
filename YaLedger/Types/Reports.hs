4445690950f0cadd3b1a04fe6fe648e7e850b70c
Ilya Portnov <portnov@iportnov.ru>
2012-10-07 14:06:44 +0600

    A bit more docs.

--- YaLedger/Types/Reports.hs
+++ YaLedger/Types/Reports.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE EmptyDataDecls, MultiParamTypeClasses, FlexibleContexts, FlexibleInstances, TypeOperators, ScopedTypeVariables, ExistentialQuantification, OverlappingInstances, TypeFamilies #-}
+-- | This module contains basic interfaces descriptions for reports.
 module YaLedger.Types.Reports
   (ReportClass (..), ReportParameter (..),
    Report (..),
@@ -20,10 +21,12 @@ import YaLedger.Types.Transactions
 import YaLedger.Monad
 import YaLedger.Kernel.Common
 
+-- | Simple command line parser monad
 newtype Parser l a = Parser {
     runParser :: [String] -> Ledger l (a, [String])
   }
 
+-- | Trivial instance
 instance Monad (Parser l) where
   return x = Parser (\s -> return (x, s))
 
@@ -31,42 +34,58 @@ instance Monad (Parser l) where
     (y, s') <- runParser m s
     runParser (f y) s'
 
+-- | Trivial instance
 instance Functor (Parser l) where
   fmap = liftM
 
+-- | This is similar to Control.Monad.Trans.lift,
+-- but with more precise type.
 liftL :: Ledger l a -> Parser l a
 liftL action = Parser $ \s -> do
   y <- action
   return (y, s)
 
+-- | Get one word from command line
 word :: Throws InvalidCmdLine l => Parser l String
 word = Parser $ \s ->
          case s of
            [] -> throw (InvalidCmdLine "Not enough parameters")
            (x:xs) -> return (x, xs)
 
-maybeWord :: Parser l (Maybe String)
-maybeWord = Parser $ \s ->
-              case s of
-                [] -> return (Nothing, [])
-                (x:xs) -> return (Just x, xs)
-
+-- | Parseable report command-line parameter
 class ReportParameter a where
   parseParameter :: (Throws InvalidCmdLine l,
                      Throws InternalError l)
                  => Parser l a
 
 class (ReportParameter (Parameters a)) => ReportClass a where
+  -- | Report options (like @-z@), parsed with GetOpt
   type Options a
+  -- | Report parameters, parsed with report's specific parser
   type Parameters a
 
+  -- | GetOpt options
   reportOptions :: a -> [OptDescr (Options a)]
+  reportOptions _ = []
+
+  -- | Default set of options
   defaultOptions :: a -> [Options a]
+  defaultOptions _ = []
+
+  -- | Main function of this class.
+  -- It usually outputs report to stdout.
   runReport :: (Throws InvalidCmdLine l,
                 Throws InternalError l)
-            => a -> Query -> [Options a] -> Parameters a -> Ledger l ()
+            => a             -- ^ Report itself; usually this is a dummy parameter, for typechecker only
+            -> Query         -- ^ Which records to process
+            -> [Options a]   -- ^ Report options, parsed by GetOpt
+            -> Parameters a  -- ^ Report parameters, parsed by report's specific parser
+            -> Ledger l ()
+
+  -- | Description of report and it's parameters/options.
   reportHelp :: a -> String
 
+-- | Container type for any report.
 data Report =
   forall a. (ReportClass a) => Report a
 
@@ -107,9 +126,13 @@ instance ReportParameter AnyAccount where
           (\l (e :: InvalidPath) ->
                rethrow l (InvalidCmdLine $ show e))
 
+-- | Run any report
 runAReport :: (Throws InvalidCmdLine l,
                Throws InternalError l)
-           => Query -> [String] -> Report -> Ledger l ()
+           => Query        -- ^ Which records to process
+           -> [String]     -- ^ Report's command line
+           -> Report       -- ^ Report itself
+           -> Ledger l ()
 runAReport qry cmdline (Report r) = do
   let ropts = reportOptions r
   (options, params) <- do
